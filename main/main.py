0b14d2ee328e22ae61bc14cfa925abde7af14072
Ethan Prentice <ethanp5262@gmail.com>
2017-12-25 22:06:49 -0500

    Updated main.py to Python 3
    Cleaned up code, implemented chunked downloads using requests.

--- main/main.py
+++ main/main.py
@@ -1,86 +1,116 @@
 '''
 Created on 27 Mar 2017
-Edited on 18 Aug 2017
+Edited on 25 Dec 2017
 
 @author: Ethan Prentice
 '''
-import os,re,inspect
-import urllib
+import os
+import re
+import inspect
+import requests
 from selenium import webdriver
+import selenium.common.exceptions
 
-def get_driver(current_dir):
+
+# Initiates driver with args
+def get_driver(c_dir: str) -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("headless");
-    return webdriver.Chrome(executable_path=current_dir+'/chromedriver.exe',chrome_options=chrome_options)
+    chrome_options.add_argument("headless")
+    return webdriver.Chrome(executable_path=c_dir + '/chromedriver.exe', chrome_options=chrome_options)
 
-def get_video_url(url, driver):
+
+# Scrapes the web page to get the URL of the raw .mp4 file
+def get_video_url(url: str, driver: webdriver.Chrome) -> str:
     try:
-        print "Fetching HTML"
+        print("Fetching HTML")
         driver.get(url)
-        print "Fetching iFrame data"
+        print("Fetching iFrame data")
         driver.switch_to.frame(driver.find_elements_by_xpath('//*[@id="body"]/iframe')[0])
         driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="video"]/iframe'))
         script_text = driver.find_element_by_xpath('/html/body/script[4]').get_attribute("innerHTML")
-        
-        mp4_url = re.findall("https://[^\"]*.mp4[^\"]*", script_text)[0]
-        
-        return mp4_url
-    
-    except Exception:
-        print "No MP4 for this URL was found %s" % url
-        return False
-        
-def download_file(url, file_dir):
-    print "\nDownloading File..."
-    testfile = urllib.URLopener()
-    testfile.retrieve(url, file_dir)
-    print "MP4 Downloaded to %s\n" % file_dir
-    
-def get_save_directory(base_dir):
-    save_name=raw_input("File Name: ")
-    if not re.match(".*.mp4", save_name, re.I):
-        save_name+=(".mp4")
-    return base_dir+"/"+save_name
 
-def does_dir_exist(base_dir):
-    if os.path.isdir(base_dir):
+        mp4_url = re.findall("https://[^\"]*.mp4[^\"]*", script_text)[0]
+
+        return mp4_url
+
+    except selenium.common.exceptions.WebDriverException:
+        print(f"Error connecting to webpage {url}")
+        return ""
+    except IndexError:
+        print(f"No MP4 for this URL was found {url}")
+        return ""
+    except Exception as e:
+        print(f"An unknown error occured, {e}")
+        return ""
+
+
+def download_file(url, download_dir):
+    print("\nDownloading File...")
+
+    # Downloads video to directory in 1 megabyte chunks
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(download_dir, 'wb') as f:
+            for chunk in r.iter_content(1024):
+                f.write(chunk)
+
+    print(f"MP4 Downloaded to {download_dir}", end='\n\n')
+
+
+# Asks user for directory to save the videos to
+def get_save_directory(directory):
+    save_name = input("File Name: ")
+    if not re.match(".*.mp4", save_name, re.I):
+        save_name += ".mp4"
+    return directory + "/" + save_name
+
+
+# Checks if the filesystem contains the given save directory
+def does_dir_exist(directory):
+    if os.path.isdir(directory):
         return True
     else:
-        create_dir = raw_input("Directory does not exist. Create directory? (y/n): ")
+        create_dir = input("Directory does not exist. Create directory? (y/n): ")
         if create_dir is not 'y':
             return False
         else:
-            os.makedirs(base_dir)
-            print "Directory Created."
+            os.makedirs(directory)
+            print("Directory Created.")
             return True
 
-os.system('cls')
-print "Loading Drivers."
-current_dir=os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-driver = get_driver(current_dir)
 
-recommended_dir=str(os.path.expanduser('~/Videos')+"/4917").replace("\\","/")
-print "Example Directory: %s" % recommended_dir
-base_dir=raw_input("Save Directory: ")
-
-if not does_dir_exist(base_dir):
-    exit(0)
-
-finished=False
-while not finished:
+def main():
     os.system('cls')
-    print "Current Directory: %s" % base_dir
-    file_dir=get_save_directory(base_dir)
-    
-    base_url=raw_input("URL: ")
-    
-    mp4_url = get_video_url(url=base_url, driver=driver)
-    if mp4_url is not False:
-        download_file(url=mp4_url, file_dir=file_dir)
-    
-    finished_input=raw_input("More URLs? (y/n): ")
-    if finished_input != 'y':
-        finished=True
-    
-print "\nExiting Application"
-driver.quit()
+    print("Loading Drivers")
+    current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    driver = get_driver(current_dir)
+
+    # Gets recommended download directory
+    recommended_dir = str(os.path.expanduser('~/Videos') + "/4917").replace("\\", "/")
+    print(f"Example Directory: {recommended_dir}")
+    base_dir = input("Save Directory: ")
+
+    if not does_dir_exist(base_dir):
+        exit(-1)
+
+    finished = False
+    while not finished:
+        os.system('cls')
+        print(f"Current Directory: {base_dir}")
+        file_dir = get_save_directory(base_dir)
+
+        base_url = input("URL: ")
+
+        mp4_url = get_video_url(url=base_url, driver=driver)
+        if mp4_url is not False:
+            download_file(url=mp4_url, download_dir=file_dir)
+
+        finished_input = input("More URLs? (y/n): ")
+        if finished_input != 'y':
+            finished = True
+
+    print("\nExiting Application")
+    driver.quit()
+
+
+main()
